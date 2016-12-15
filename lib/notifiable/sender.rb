5d89d9ea8e61355381c49aaab4e82a4c9b7c6100
Matt Brooke-Smith <matt@futureworkshops.com>
2016-12-15 11:53:29 +0000

    Accept ruby arrays/hashs for parameters / filters

--- lib/notifiable/sender.rb
+++ lib/notifiable/sender.rb
@@ -12,14 +12,14 @@ module Notifiable
       end
       
       def send_notification_to_user(user_alias, message: nil, parameters: nil)
-        send_notification(message: message, parameters: parameters, filters: "[{\"property\": \"user_alias\", \"predicate\": \"eq\", \"value\": \"#{user_alias}\"}]")
+        send_notification(message: message, parameters: parameters, filters: [{property: "user_alias", predicate: "eq", value: user_alias}])
       end
       
       def send_notification(message: nil, parameters: nil, filters: nil, content_avaliable: nil)
         query = {}
         query[:message] = message if message
-        query[:parameters] = parameters if parameters
-        query[:filters] = filters if filters
+        query[:parameters] = parameters.to_json if parameters
+        query[:filters] = filters.to_json if filters
         query[:content_avaliable] = content_avaliable if content_avaliable
         self.class.post("#{@base_uri}/api/v1/notifications", 
           query: { notification: query },
--- spec/notifiable/sender_v1_spec.rb
+++ spec/notifiable/sender_v1_spec.rb
@@ -4,52 +4,58 @@ describe Notifiable::Sender::V1 do
   
   let(:access_id) { "abc123" }
   let(:base_uri) { "http://notifiable.com" }
-  let(:query) { {} }
+  let(:args) { {} }
+  let(:notification_query_params) { args }
   subject { Notifiable::Sender::V1.new(base_uri, access_id) }
   
   describe "#send_notification" do
     before(:each) do
       stub_request(:post, "http://notifiable.com/api/v1/notifications").
-               with(query: {notification: query}).
+               with(query: {notification: notification_query_params}).
                with(:headers => {'Authorization'=>'abc123'}).
                to_return(status: 200)
     
-      @response = subject.send_notification(query)
+      @response = subject.send_notification(args)
     end    
     
     context "with message" do
-      let(:query) { {message: "New Offers"} }
+      let(:args) { {message: "New Offers"} }
       it { expect(@response.code).to eq 200 }      
     end
     
     context "with parameters" do
-      let(:query) { {parameters: "{flow_id: 5}"} }
+      let(:args) { {parameters: {flow_id: 5}} }
+      let(:notification_query_params) { {parameters: "{\"flow_id\":5}"} }
       it { expect(@response.code).to eq 200 }      
     end
     
     context "with filters" do
-      let(:query) { {filters: "[{\"property\": \"lat\", \"predicate\": \"lt\", \"value\": 40.3}]"} }      
+      let(:args) { {filters: [{"property": "lat", "predicate": "lt", "value": 40.3}]} } 
+      let(:notification_query_params) { {filters: "[{\"property\":\"lat\",\"predicate\":\"lt\",\"value\":40.3}]"} }     
       it { expect(@response.code).to eq 200 }      
     end
     
     context "with content_avaliable" do
-      let(:query) { {content_avaliable: true} }      
+      let(:args) { {content_avaliable: true} }      
       it { expect(@response.code).to eq 200 }      
     end
   end
   
   describe "#send_notification_to_user" do
     let(:user_alias) { "matt@futureworkshops.com" }
-    let(:additional_query) { {} }
-    let(:query) { additional_query.merge({filters: "[{\"property\": \"user_alias\", \"predicate\": \"eq\", \"value\": \"#{user_alias}\"}]"}) }
+    let(:args) { {} }
+    let(:notification_query_params) { additional_notification_query_params.merge({filters: "[{\"property\":\"user_alias\",\"predicate\":\"eq\",\"value\":\"#{user_alias}\"}]"}) }
+    let(:additional_notification_query_params) { {} }
+    
+    let(:query) { {} }
     
     before(:each) do
       stub_request(:post, "http://notifiable.com/api/v1/notifications").
-               with(query: {notification: query}).
+               with(query: {notification: notification_query_params}).
                with(:headers => {'Authorization'=>'abc123'}).
                to_return(status: 200)
     
-      @response = subject.send_notification_to_user(user_alias, additional_query)
+      @response = subject.send_notification_to_user(user_alias, args)
     end 
     
     context "with message" do
