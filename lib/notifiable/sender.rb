4d670b05ea6d050d43aa9b47a1397b24999594ad
Matt Brooke-Smith <matt@futureworkshops.com>
2018-03-01 18:49:45 +0000

    Update namespacing

--- lib/notifiable/sender.rb
+++ lib/notifiable/sender.rb
@@ -5,37 +5,34 @@ require 'json'
 require 'logger'
 
 module Notifiable
-  module Sender
-    class V1
-      def initialize(base_uri, access_id, secret_key: nil, logger: Logger.new(STDOUT))
-        raise 'base_uri cannot be nil' if base_uri.empty?
-        raise 'access_id cannot be nil' if access_id.empty? 
-        
-        @base_uri, @access_id, @secret_key, @logger = base_uri, access_id, secret_key, logger
-      end
+  class Sender
+    def initialize(base_uri, access_id, secret_key: nil, logger: Logger.new(STDOUT))
+      raise 'base_uri cannot be nil' if base_uri.empty?
+      raise 'access_id cannot be nil' if access_id.empty? 
       
-      def send_notification_to_user(user_alias, message: nil, parameters: nil, content_available: nil)
-        filters = [{property: "user_alias", predicate: "eq", value: user_alias}]
-        send_notification(message: message, parameters: parameters, content_available: content_available, filters: filters)
+      @base_uri, @access_id, @secret_key, @logger = base_uri, access_id, secret_key, logger
+    end
+    
+    def send_notification_to_user(user_alias, message: nil, parameters: nil, content_available: nil)
+      filters = [{property: "user_alias", predicate: "eq", value: user_alias}]
+      send_notification(message: message, parameters: parameters, content_available: content_available, filters: filters)
+    end
+    
+    def send_notification(message: nil, parameters: nil, filters: nil, content_available: nil)
+      body = {}
+      body[:message] = message unless message.nil?
+      body[:parameters] = parameters.to_json unless parameters.nil?
+      body[:filters] = filters.to_json unless filters.nil?
+      body[:content_available] = content_available unless content_available.nil?
+      headers = {}
+      headers[:authorization] = @access_id unless @secret_key
+      @request = RestClient::Request.new url: "#{@base_uri}/api/v1/notifications", payload: {notification: body}, method: :post, headers: headers
+      @request = ApiAuth.sign!(@request, @access_id, @secret_key) if @secret_key
+      begin
+        @request.execute
+      rescue RestClient::ExceptionWithResponse => e
+        @logger.warn "Request failed with code: #{e.response.code} body: #{e.response.body}"
       end
-      
-      def send_notification(message: nil, parameters: nil, filters: nil, content_available: nil)
-        body = {}
-        body[:message] = message unless message.nil?
-        body[:parameters] = parameters.to_json unless parameters.nil?
-        body[:filters] = filters.to_json unless filters.nil?
-        body[:content_available] = content_available unless content_available.nil?
-        headers = {}
-        headers[:authorization] = @access_id unless @secret_key
-        @request = RestClient::Request.new url: "#{@base_uri}/api/v1/notifications", payload: {notification: body}, method: :post, headers: headers
-        @request = ApiAuth.sign!(@request, @access_id, @secret_key) if @secret_key
-        begin
-          @request.execute
-        rescue RestClient::ExceptionWithResponse => e
-          @logger.warn "Request failed with code: #{e.response.code} body: #{e.response.body}"
-        end
-      end
-      
     end
   end
 end
--- lib/notifiable/sender/version.rb
+++ lib/notifiable/sender/version.rb
@@ -1,5 +1,5 @@
 module Notifiable
   module Sender
-    VERSION = "0.1.0"
+    VERSION = "0.2.0"
   end
 end
